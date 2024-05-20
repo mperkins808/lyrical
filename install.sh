866bf12400f9117abbec3346125c1f522991ceea
Mathew Perkins <mathewperkins@Mathews-MacBook-Pro.local>
2024-05-20 21:47:49 +1000

    feat: bump release

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 GITHUB_REPO="mperkins808/lyrical"
 BINARY_NAME="lyrical"
-TAG="0.0.1"
+TAG="0.0.2"
 BINARY_URL="https://github.com/$GITHUB_REPO/releases/download/$TAG/$BINARY_NAME"
 
 TEMP_DIR=$(mktemp -d)
